c53d61efbbd9ec6451e3ecf8c065d8750a26c799
Greg Magarshak <greg@qbix.com>
2025-12-10 16:14:27 +0400

    Fixed query in previous refactor

--- handlers/Metrics/before/Users_User_lastActiveTime.php
+++ handlers/Metrics/before/Users_User_lastActiveTime.php
@@ -7,10 +7,11 @@ function Metrics_before_Users_User_lastActiveTime($params, &$result)
 	$userId = $params['userId'];
 
 	// Try to find last visit
-	$visits = Metrics_Visit::select(array(
+	$visits = Metrics_Visit::select()->where(array(
 		'id' => new Db_Range($userId . ';', true, false, true)
 	))->orderBy('lastTime', false)->limit(1)->fetchDbRows();
 	if ($visits) {
+
 		$visit = $visits[0];
 		if ($visit->lastTime) {
 			$result = strtotime($visit->lastTime);
